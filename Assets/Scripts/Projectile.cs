f686ad20bd1eb66cfa9f53c0c8c746e848e0b391
Jason Petterson <jp@brokenshotgun.com>
2015-09-23 19:34:49 -0400

    added color randomize to projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -10,6 +10,14 @@ public class Projectile : MonoBehaviour {
 
     void Update() {
         transform.Rotate(Vector3.forward, -10f);
+        RandomizeColor();
+    }
+
+    private void RandomizeColor() {
+        float r = Random.Range(0f, 1f);
+        float g = 0f; //Random.Range(0f, 1f);
+        float b = 0f; //Random.Range(0f, 1f);
+        GetComponent<SpriteRenderer>().color = new Color(r, g, b);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
